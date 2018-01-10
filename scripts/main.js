d916cf9e7ee123c7ef01f0776cc61a8955c9c07a
William Hamilton <wrhamilton1@gmail.com>
2018-01-09 23:50:52 -0500

    Made several JS changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.jpg') {
+      myImage.setAttribute ('src','images/avengers.jpg');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
